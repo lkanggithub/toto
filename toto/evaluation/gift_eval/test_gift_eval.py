d10c7557054b5b061e7cdc5d406a77f498a7e30d
Lingjun Kang <lyndon.kang@datarobot.com>
2025-07-22 08:36:09 -0400

    setup context length in model

--- toto/evaluation/gift_eval/test_gift_eval.py
+++ toto/evaluation/gift_eval/test_gift_eval.py
@@ -50,7 +50,7 @@ PRETTY_DATASET_NAMES = {
     "car_parts_with_missing": "car_parts",
 }
 
-SHORT_DATASETS = "m4_yearly m4_quarterly m4_monthly m4_weekly m4_daily m4_hourly electricity/15T electricity/H electricity/D electricity/W solar/10T solar/H solar/D solar/W hospital covid_deaths us_births/D us_births/M us_births/W saugeenday/D saugeenday/M saugeenday/W temperature_rain_with_missing kdd_cup_2018_with_missing/H kdd_cup_2018_with_missing/D car_parts_with_missing restaurant hierarchical_sales/D hierarchical_sales/W LOOP_SEATTLE/5T LOOP_SEATTLE/H LOOP_SEATTLE/D SZ_TAXI/15T SZ_TAXI/H M_DENSE/H M_DENSE/D ett1/15T ett1/H ett1/D ett1/W ett2/15T ett2/H ett2/D ett2/W jena_weather/10T jena_weather/H jena_weather/D bitbrains_fast_storage/5T bitbrains_fast_storage/H bitbrains_rnd/5T bitbrains_rnd/H bizitobs_application bizitobs_service bizitobs_l2c/5T bizitobs_l2c/H"
+SHORT_DATASETS = "electricity/D electricity/W solar/10T solar/H solar/D solar/W hospital covid_deaths us_births/D us_births/M us_births/W saugeenday/D saugeenday/M saugeenday/W temperature_rain_with_missing kdd_cup_2018_with_missing/H kdd_cup_2018_with_missing/D car_parts_with_missing restaurant hierarchical_sales/D hierarchical_sales/W LOOP_SEATTLE/5T LOOP_SEATTLE/H LOOP_SEATTLE/D SZ_TAXI/15T SZ_TAXI/H M_DENSE/H M_DENSE/D ett1/15T ett1/H ett1/D ett1/W ett2/15T ett2/H ett2/D ett2/W jena_weather/10T jena_weather/H jena_weather/D bitbrains_fast_storage/5T bitbrains_fast_storage/H bitbrains_rnd/5T bitbrains_rnd/H bizitobs_application bizitobs_service bizitobs_l2c/5T bizitobs_l2c/H"
 # MED_LONG_DATASETS = "electricity/15T electricity/H solar/10T solar/H kdd_cup_2018_with_missing/H LOOP_SEATTLE/5T LOOP_SEATTLE/H SZ_TAXI/15T M_DENSE/H ett1/15T ett1/H ett2/15T ett2/H jena_weather/10T jena_weather/H bitbrains_fast_storage/5T bitbrains_rnd/5T bizitobs_application bizitobs_service bizitobs_l2c/5T bizitobs_l2c/H"
 MED_LONG_DATASETS = "bizitobs_l2c/H"
 
@@ -462,12 +462,8 @@ def evaluate_dataset_with_model(model, task: EvalTask) -> pd.DataFrame:
     # Simply use the already prettified dataset key with frequency and term
     ds_config = f"{task.dataset_key}/{task.dataset_frequency}/{task.term}"
 
-    if not task.pad_short_series:
-        context_length = min(
-            DEFAULT_CONTEXT_LENGTH, get_maximal_context_length(dataset)
-        )
-    else:
-        context_length = DEFAULT_CONTEXT_LENGTH
+    context_length = dataset.prediction_length
+    print(f"Model context length: {context_length}")
 
     # Set up evaluation metrics - create all metrics from the config
     metrics, _ = init_metrics()
